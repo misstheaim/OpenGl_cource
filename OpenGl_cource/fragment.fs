b642b8921b17c54a3ebad64d62d51f84d368791c
misstheaim <126205386+misstheaim@users.noreply.github.com>
2024-03-10 23:10:33 +0500

    matrix update

--- OpenGl_cource/fragment.fs
+++ OpenGl_cource/fragment.fs
@@ -1,8 +1,14 @@
 #version 330 core
 out vec4 FragColor;  
 in vec3 ourColor;
+in vec2 TexCoord;
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+uniform float trans;
   
 void main()
 {
-    FragColor = vec4(ourColor, 1.0);
+    FragColor = mix(texture(texture2, vec2(1.0 - TexCoord.x, TexCoord.y)), texture(texture1, TexCoord), 0.8);//texture(texture2, TexCoord);// * vec4(ourColor, 1.0);
+     
 }
--- OpenGl_cource/main.cpp
+++ OpenGl_cource/main.cpp
@@ -2,6 +2,12 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+
+#include "stb_image.h"
+
 #include "shader_s.h"
 
 
@@ -93,19 +99,36 @@ int main() {
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);*/
 	Shader ourShader("vertex.vs", "fragment.fs");
-	//---------------
+	//---------------											 
+															
 
+	glm::mat4 transf = glm::mat4(1.0f);
+	transf = glm::translate(transf, glm::vec3(0.5f, -0.5f, 0.0f));
+	//transf = glm::rotate(transf, glm::radians(45.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+	transf = glm::scale(transf, glm::vec3(0.5f, 0.5f, 0.0f));
+	
 
 	// Creating a buffer of vertices
-	float vertices[] = {
-		-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-		0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
-		0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
-		-0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 1.0f
+	/*float vertices[] = {
+		-0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,
+		0.5f, -0.5f, 0.0f,    0.0f, 1.0f, 0.0f,   1.0f, 0.0f,
+		0.5f, 0.5f, 0.0f,     0.0f, 0.0f, 1.0f,   0.0f, 0.0f,
+		-0.5f, 0.5f, 0.0f,    1.0f, 1.0f, 1.0f,   0.0f, 1.0f   
 	};
 	unsigned int indices[] = {
 		0, 1, 2,
 		0, 2, 3
+	};*/
+	float vertices[] = {
+		// positions          // colors           // texture coords
+		 1.0f,  1.0f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+		 1.0f, -1.0f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+		-1.0f, -1.0f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+		-1.0f,  1.0f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
+	};
+	unsigned int indices[] = {
+		0, 1, 3, // first triangle
+		1, 2, 3  // second triangle
 	};
 
 	unsigned int VBO, VAO, EBO;
@@ -121,11 +144,66 @@ int main() {
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 	glEnableVertexAttribArray(0);
 	glEnableVertexAttribArray(1);
+	glEnableVertexAttribArray(2);
 	//-----------
+
+	//Creating texture
+	unsigned int texture1;										 
+	glGenTextures(1, &texture1);
+	glBindTexture(GL_TEXTURE_2D, texture1);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	stbi_set_flip_vertically_on_load(true);
+
+	int width, height, nrChannels;
+	unsigned char* data = stbi_load("awesomeface.png", &width, &height, &nrChannels, 0);
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else {
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+
+	unsigned int texture2;
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else {
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+	//------------------
+
+	ourShader.use();
+	ourShader.setInt("texture1", 0);
+	ourShader.setInt("texture2", 1);
+	float trans = 0.8f;
+
+	unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
+
+	float degree = 45.0f;
+	float degree1 = 45.0f;
 
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
@@ -133,16 +211,35 @@ int main() {
 		glClearColor(0.1f, 0.7f, 0.7f, 0);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		float timeValue = glfwGetTime();
+		degree = sin(timeValue/2.0f) + 0.1f;
+		transf = glm::rotate(transf, glm::radians(degree), glm::vec3(0.0f, 0.0f, 1.0f));
+		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transf));
+
 		/*float timeValue = glfwGetTime();
 		float greenValue = sin(timeValue / 2.0f) + 0.5f;
 		int ourColorLocation = glGetUniformLocation(shaderProgram, "ourColor");*/
 
 		//glUseProgram(shaderProgram);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture2);
 		ourShader.use();
 		//glUniform4f(ourColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+		
 		glBindVertexArray(VAO);
 		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		glm::mat4 transf1 = glm::mat4(1.0f);
+		transf1 = glm::translate(transf1, glm::vec3(-0.5f, 0.5f, 0.0f));
+		//transf1 = glm::scale(transf1, glm::vec3(degree, degree, 0.0f));
+		degree1++;
+		transf1 = glm::rotate(transf1, glm::radians(degree1), glm::vec3(0.0f, 0.0f, 1.0f));
+		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transf1));
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
 		glBindVertexArray(0);
 		//glDrawArrays(GL_TRIANGLES, 0, 3);
 
